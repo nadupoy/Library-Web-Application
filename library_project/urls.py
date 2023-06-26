4793e8a25683f8fb596fdfa1fe00fe65b75441b5
nadupoy <gnsampao@gmail.com>
2023-06-26 11:23:56 +0300

    First commit.

---
+++ library_project/urls.py
@@ -0,0 +1,30 @@
+"""
+URL configuration for library_project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from library_app import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home_page, name='home'),
+    path('library/', views.library_page, name='library'),
+    path('library/details/', views.book_details_page, name='details')
+    # path('library/author/', name='author'),
+    # path('library/genre/', name='genre'),
+    # path('library/availability', name='availability'),
+]
