b913ca97ae6f453d6a7325ff9f5557271fc6cb92
nadupoy <gnsampao@gmail.com>
2023-06-30 20:36:33 +0300

    Changed the 'upload_to' attribute value to 'media' for the thumbnail property in the Book class.

--- library_app/models.py
+++ library_app/models.py
@@ -32,7 +32,7 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey("Author", on_delete=models.CASCADE)
     genre = models.CharField(max_length=7, choices=GENRES)
-    thumbnail = models.ImageField(upload_to="book_covers")
+    thumbnail = models.ImageField(upload_to="media")
     blurb = models.CharField(max_length=100)
     # availability = # *an input field, so it should probably be in its own class and be externally referenced.
 
