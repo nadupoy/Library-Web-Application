f32dacfbab3671245a8d65bf8ce22c013065317d
nadupoy <gnsampao@gmail.com>
2023-06-30 20:39:42 +0300

    Added url patterns for the media files.

--- library_project/urls.py
+++ library_project/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from library_app import views
+from library_project import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,4 +30,7 @@ urlpatterns = [
     # path('library/author/', name='author'),
     # path('library/genre/', name='genre'),
     # path('library/availability', name='availability'),
-]
+] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
