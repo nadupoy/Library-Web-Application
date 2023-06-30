940683615cee9ba4ec17c41f1602a99aa76a71b8
nadupoy <gnsampao@gmail.com>
2023-06-30 20:34:06 +0300

    Customized 'Books' to list the title, author and genre.'

--- library_app/admin.py
+++ library_app/admin.py
@@ -3,5 +3,8 @@ from django.contrib import admin
 # Register your models here.
 from .models import Book, Author
 
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ["title", "author", "genre"]
+
+admin.site.register(Book, BookAdmin)
 admin.site.register(Author)
