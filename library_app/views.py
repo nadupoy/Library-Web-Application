da2b0b2ec60155447eae4bbdb63092b18794fe48
nadupoy <gnsampao@gmail.com>
2023-07-08 12:21:52 +0300

    Added 'book_details_page' view function that maps onto a dynamic URL.

--- library_app/views.py
+++ library_app/views.py
@@ -2,15 +2,22 @@ from django.shortcuts import render
 
 from .models import Book
 
+
 def home_page(request):
-    return render(request, "library_app/index.html")
+    return render(request, 'library_app/index.html')
+
 
 def library_page(request):
     books = Book.objects.all()
     context = {
-        "books": books,
+        'books': books,
         }
-    return render(request, "library_app/library.html", context)
+    return render(request, 'library_app/library.html', context)
 
-def book_details_page(request):
-    return render(request, "library_app/details.html")
+
+def book_details_page(request, book_title):
+    book = Book.objects.get(title=book_title)
+    context = {
+        'book': book,
+    } 
+    return render(request, 'library_app/details.html', context)
